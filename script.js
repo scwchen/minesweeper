25c361117320ce157dd45d70e5cdb05ced7d258c
Steven Chen <steven.chen.121@gmail.com>
2021-11-16 22:16:24 -0500

    Added number of mines on grid depending on Mine placement.

--- script.js
+++ script.js
@@ -6,86 +6,149 @@ const app = {};
 app.grid = document.querySelector('.playgrid');
 app.difficulty = 'beginner';
 // will be an array of objects
-// app.squareInfo = [];
-app.minesSquares = [];
+app.squareInfo = [];
+app.width = 0;
+app.height = 0;
 
 app.gameInfo =
 {
     beginner: {
         width: 9,
         height: 9,
-        mines: 10,
+        numMines: 10,
     },
     intermediate: {
         width: 16,
         height: 16,
-        mines: 40,
+        numMines: 40,
     },
     expert: {
         width: 30,
         height: 16,
-        mines: 99,
+        numMines: 99,
     }
 };
 
 app.setDifficulty = () => {
     const r = document.querySelector(':root');
     const gameType = app.gameInfo[app.difficulty];
-    const { width, height, mines } = gameType;
+    const { width, height, numMines } = gameType;
+    app.width = width;
+    app.height = height;
 
-    r.style.setProperty('--grid-width', width);
-    r.style.setProperty('--grid-height', height);
+    r.style.setProperty('--grid-width', app.width);
+    r.style.setProperty('--grid-height', app.width);
 
-    app.makeGrid(width, height);
-    app.setMines(mines);
+    app.makeGrid();
+    app.setMines(numMines);
+    app.setNumbers();
+}
+
+app.getRandom = (max) => {
+    const newRandom = Math.floor(Math.random() * max);
+    const mineArray = app.squareInfo.filter(square => square.mine);
+
+    if (!mineArray.includes(newRandom)) {
+        return newRandom;
+    } else {
+        return app.getRandom(max);
+    }
 }
 
 app.setMines = (mines) => {
-    const squares = [...document.querySelectorAll('.playsquare')];
-    const squaresRest = [...squares];
+    const squares = document.querySelectorAll('.playsquare');
 
     for (let i = 0; i < mines; i++) {
         // number between 0 and 80 for the indices of the nodelist
-        const newIndex = Math.floor(Math.random() * squaresRest.length);
-        app.minesSquares.push(newIndex);
-        squaresRest.splice(newIndex, 1);
+        const newIndex = app.getRandom(squares.length);
+        app.squareInfo[newIndex].mine = true;
     }
 
-    app.minesSquares.forEach(mine => {
-        squares[mine].classList.add('mine');
+    app.squareInfo.filter(square => square.mine).map(square => {
+        squares[square.id].classList.add('mine');
     });
 }
 
-app.makeGrid = (gridWidth, gridHeight) => {
+app.makeGrid = () => {
 
-    for (let i = 1; i <= (gridWidth * gridHeight); i++) {
+    for (let i = 0; i < (app.width * app.height); i++) {
         const square = document.createElement('button');
         square.classList.add('playsquare');
+        square.id = i;
         app.grid.append(square);
+        app.squareInfo.push({
+            id: i,
+            mine: false,
+            flag: false,
+            number: 0,
+        })
     }
+
+
 }
 
 app.setNumbers = () => {
+    const squares = document.querySelectorAll('.playsquare');
+    const mines = app.squareInfo.filter(square => square.mine);
+
+    mines.forEach(square => {
+        const mine = square.id;
+        const left = mine - 1;
+        const right = mine + 1;
+        const up = mine - app.width;
+        const down = mine + app.width;
+        const maxIndex = app.width * app.height - 1;
+
+        // if left of the mine is still on the grid, not on a mine, and on the same line
+        if (left >= 0 && !mines.includes(left)) {
+            app.squareInfo[left].number += 1;
+        }
+        if (right <= maxIndex && !mines.includes(right)) {
+            app.squareInfo[right].number += 1;
+        }
+        if (up >= 0 && !mines.includes(up)) {
+            app.squareInfo[up].number += 1;
+        }
+        if (down <= maxIndex && !mines.includes(down)) {
+            app.squareInfo[down].number += 1;
+        }
+    });
+
+    app.squareInfo.forEach((square) => {
+        if (square.number > 0) {
+            squares[square.id].innerText = `${square.number}`;
+        }
+
+    })
 
 }
 
 app.handleSquareClick = () => {
-    const squares = document.querySelectorAll('.playsquare');
+
     const grid = document.querySelector('.playgrid');
+    const squares = document.querySelectorAll('.playsquare');
 
     grid.addEventListener('contextmenu', e => e.preventDefault());
 
     squares.forEach(square => {
         square.addEventListener('contextmenu', (e) => {
+            app.squareInfo[e.target.id].flag = true;
             square.classList.toggle('flag');
-        })
+        });
+        square.addEventListener('click', (e) => {
+            app.showSquare(e.target.id);
+        });
     })
 };
+
+app.showSquare = (square) => {
+    const squares = document.querySelectorAll('.playsquare');
+
+}
 
 app.init = () => {
     app.setDifficulty();
     app.handleSquareClick();
-    console.log(app.minesSquares.sort((a, b) => a - b));
 };
 
 app.init();
